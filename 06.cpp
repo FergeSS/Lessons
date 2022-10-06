ed1d0db8f9700fa6e60da46e6a178c0d4d1df9ea
FergeSS <97806343+FergeSS@users.noreply.github.com>
2022-10-06 16:42:32 +0300

    Classwork

--- 06.cpp
+++ 06.cpp
@@ -1,9 +1,68 @@
 #include <iostream>
+int task1(){
+    int n, i, summ = 0;
+    
+    std::cout << "Input N: ";
+    std::cin >> n;
+    
+    for(i = 1; i<=n; i++)
+        summ += i;
+        
+    std::cout << double(summ) / n << std::endl;
+    
+    return 0;
+}
+
+int task2(){
+    int n, k, i;
+    
+    std::cout << "Input N: ";
+    std::cin >> n;  
+    
+    for(i = 1; i <= n; i++){
+        for(k = 0; k < i; k++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
+
+int task3(){
+    int n, k, i, j;
+    
+    std::cout << "Input N: ";
+    std::cin >> n; 
+    std::cout << "\n\n";
+    
+    for(i = 1; i <= n; i++){
+        for(k = 0; k < n - i; k++){
+            std::cout << " ";
+        }
+        for(j = 1; j < i * 2; j++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main(){
-  int count = 0;
-  while (count < 5){
-    std::cout << count << std::endl;
-    count++;
-  }
+    int t;
+    
+    std::cout << "Input task: ";
+    std::cin >> t;
+    
+    if(t == 1){
+        task1();
+    }
+    
+    if(t == 2){
+        task2();
+    }
+    
+    if(t == 3){
+        task3();
+    }
+    return 0;
 }
+
