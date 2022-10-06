296cfe808c719da1995974f9f6a8ef523fc5fd65
FergeSS <97806343+FergeSS@users.noreply.github.com>
2022-10-06 16:47:09 +0300

    +task4

--- 06.cpp
+++ 06.cpp
@@ -46,6 +46,33 @@ int task3(){
     }
 }
 
+int task4(){
+    int n, k, i, j;
+    
+    std::cout << "Input N: ";
+    std::cin >> n; 
+    std::cout << "\n\n";
+    
+    for(i = 1; i <= n; i++){
+        for(k = 0; k < n - i; k++){
+            std::cout << " ";
+        }
+        for(j = 1; j < i * 2; j++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    for(i = 1; i <= n - 1; i++){
+        for(k = n; k > n - i; k--){
+            std::cout << " ";
+        }
+        for(j = n * 2 - 1; j > i * 2; j--){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main(){
     int t;
     
@@ -62,6 +89,10 @@ int main(){
     
     if(t == 3){
         task3();
+    }
+    
+    if(t == 4){
+        task4();
     }
     return 0;
 }
